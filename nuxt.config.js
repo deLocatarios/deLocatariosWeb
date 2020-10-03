598fc65c91dc23f93f9c0db231dc3faddf20abb9
Victor Escalante <victorlt.xd3@gmail.com>
2020-10-03 03:33:25 -0500

    ok

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,7 @@ export default {
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
   */
+  target: 'static',
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
