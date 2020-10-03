fe004d53c71b44cca930eac08b0b43bcde472ef2
Victor Escalante <victorlt.xd3@gmail.com>
2020-10-03 01:18:36 -0500

    Update errors

--- pages/business/_id.vue
+++ pages/business/_id.vue
@@ -1,57 +1,61 @@
 <template>
   <div>
-    <Cover :title=business.name :cover_image=business.cover.url></Cover>
-    <el-row class="body-business">
-      <el-col :md="16">
+    <div v-if="$fetchState.pending">Cargando ...</div>
+    <div v-else>
+      <Cover  :title=business.name :cover_image=business.cover.url></Cover>
+      <el-row class="body-business">
+        <el-col :md="16">
 
-        <el-card class="box-card">
-          <h1 class="title">{{ business.name }}</h1>
-          <el-divider></el-divider>
-          <h4>Descripción</h4>
-          <br>
-          <p v-html="business.description"></p>
-          <el-divider></el-divider>
-          <h4>Categorías</h4>
-          <el-row class="business-categories">
-            <el-button type="primary" v-for="category in business.categories">{{ category.name }}</el-button>
-          </el-row>
-        </el-card>
+          <el-card class="box-card">
+            <h1 class="title">{{ business.name }}</h1>
+            <el-divider></el-divider>
+            <h4>Descripción</h4>
+            <br>
+            <p v-html="business.description"></p>
+            <el-divider></el-divider>
+            <h4>Categorías</h4>
+            <el-row class="business-categories">
+              <el-button type="primary" v-for="category in business.categories">{{ category.name }}</el-button>
+            </el-row>
+          </el-card>
 
-        <el-card>
-          <div slot="header" class="clearfix">
-            <span>Galería</span>
-          </div>
-          <div>
-            <el-carousel indicator-position="outside" height="500px" :autoplay=false>
-              <el-carousel-item v-for="item in business.images" :key="item.id">
-                <div class="item-image" :style=getImage(item.url)></div>
-              </el-carousel-item>
-            </el-carousel>
-          </div>
-        </el-card>
+          <el-card>
+            <div slot="header" class="clearfix">
+              <span>Galería</span>
+            </div>
+            <div>
+              <el-carousel indicator-position="outside" height="500px" :autoplay=false>
+                <el-carousel-item v-for="item in business.images" :key="item.id">
+                  <div class="item-image" :style=getImage(item.url)></div>
+                </el-carousel-item>
+              </el-carousel>
+            </div>
+          </el-card>
 
-      </el-col>
-      <el-col :md="8">
+        </el-col>
 
-        <el-card class="box-card">
-          <h4>Información de contacto</h4>
-          <div class="business-info">
-            <p v-if="business.phone"><i class="el-icon-phone"></i> {{ business.phone }}</p>
-            <p v-if="business.address"><i class="el-icon-add-location"></i> {{ business.address }}</p>
-            <p v-if="business.email"><i class="el-icon-user"></i> {{ business.email }}</p>
-          </div>
-        </el-card>
+        <el-col :md="8">
 
-        <el-card class="box-card">
-          <h4>Horario</h4>
-        </el-card>
+          <el-card class="box-card">
+            <h4>Información de contacto</h4>
+            <div class="business-info">
+              <p v-if="business.phone"><i class="el-icon-phone"></i> {{ business.phone }}</p>
+              <p v-if="business.address"><i class="el-icon-add-location"></i> {{ business.address }}</p>
+              <p v-if="business.email"><i class="el-icon-user"></i> {{ business.email }}</p>
+            </div>
+          </el-card>
 
-        <el-card class="box-card">
-          <h4>Localización</h4>
-        </el-card>
+          <el-card class="box-card">
+            <h4>Horario</h4>
+          </el-card>
 
-      </el-col>
-    </el-row>
+          <el-card class="box-card">
+            <h4>Localización</h4>
+          </el-card>
+
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
@@ -80,7 +84,7 @@
       try {
         this.business = await this.$strapi.findOne('businesses', this.$route.params.id);
       } catch (error) {
-        this.error = error
+        console.log("error", error)
       }
     }
   }
